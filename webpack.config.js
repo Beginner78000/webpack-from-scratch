f57a4085593c0380bfa449282e85e81d782e95e3
Marie HART <50957448+Beginner78000@users.noreply.github.com>
2023-01-10 18:26:27 +0100

    updated webpack.config file and babelrc

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
